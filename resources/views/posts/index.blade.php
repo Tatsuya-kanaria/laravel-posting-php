5d5d36131bc68488526850c198862c3720343bb7
Tatsuya-kanaria <kanaria.git@gmail.com>
2022-10-28 23:59:02 +0900

    first commit

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>投稿一覧</title>
+</head>
+
+<body class>
+  <!-- header -->
+  <header>
+    <nav>
+      <a href="{{ route('posts.index') }}">投稿アプリ</a>
+    </nav>
+  </header>
+
+  <!-- main -->
+  <main>
+    <article class="home">
+      <h1>投稿一覧</h1>
+    </article>
+  </main>
+
+  <!-- footer -->
+  <footer>
+    <p class="copyright">&copy; 投稿アプリ All rights reserved.</p>
+  </footer>
+</body>
+</html>
