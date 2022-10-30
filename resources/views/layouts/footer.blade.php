10d8a3a3af4ca894a77f4f932a0d78649346ffc9
Tatsuya-kanaria <kanaria.git@gmail.com>
2022-10-30 23:42:20 +0900

    add layouts app header footer

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,4 @@
+<!-- footer -->
+<footer class="d-flex justify-content-center align-items-center bg-light fixed-bottom" style="height: 60px;">
+    <p class="text-muted small mb-0">&copy; 投稿アプリ All rights reserved.</p>
+</footer>
